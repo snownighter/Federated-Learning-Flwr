1cba43810377af702b9201cde53d9fd37930f739
snownighter <daniel44209350@gmail.com>
2023-01-16 00:07:49 +0800

    FedFlandre

--- Flwr/quickstart_tensorflow/server.py
+++ Flwr/quickstart_tensorflow/server.py
@@ -2,12 +2,12 @@ import flandre as fl
 from flandre.server.strategy import FedAvg
 
 import utils as uts
-import exFedAvg as eFedAvg
+from FedFlan import FedFlan
 
 N = 2
 
 # FedAvg is the default strategy used when you start the server without a custom strategy
-strategy = FedAvg(
+strategy = FedFlan(
     min_fit_clients=N,
     min_evaluate_clients=N,
     min_available_clients=N,
